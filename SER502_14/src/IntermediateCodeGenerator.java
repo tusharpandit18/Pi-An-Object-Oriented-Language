a846add30e07113a091e731febeb9cdbbab09aff
Tushar Pandit <tushar.pandit18@gmail.com>
2017-04-26 20:05:40 -0700

    Intermediate code for print statement
    Intermediate code for print statement

--- SER502_14/src/IntermediateCodeGenerator.java
+++ SER502_14/src/IntermediateCodeGenerator.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class IntermediateCodeGenerator {
-	
+
 	/* Key - variable name
 	   array : index-0 - type, index-1 - attribute, index -3 to store value
 	   value will be assigned in runtime
-	*/
+	 */
 	static HashMap<String,String[]> symbol_table = new HashMap<String,String[]>();
 
 	public static void main(String[] args){
@@ -15,20 +15,20 @@ public class IntermediateCodeGenerator {
 		// Split the tree and store in an array
 		String instructions[] = parseTree.split("[()]+"); 
 		ArrayList<String> intermediate_code = new ArrayList<String>();
-		
-		
+
+
 		for (int  i = 0; i< instructions.length ; i++){
-					
+
 			// Ignore empty rows
 			if (! instructions[i].equals("")){
 				// Write functions for each operation
-				
+
 				if (instructions[i].trim().equals("declaration")){
 					//Ignore empty cells if any
 					while (instructions[i] == "")i++;
 					// goto next valid statement and check if it is declare or declare and assign
 					i++;
-									
+
 					if (instructions[i].split(" ")[0].equals("declare")){
 						String []row = instructions[i].split(" ");
 						String key = row[2];
@@ -37,7 +37,7 @@ public class IntermediateCodeGenerator {
 						//value[1] = "";
 						// enter in intermediate code
 						intermediate_code.add(row[0]+ " " + row[2]);
-						
+
 						// enter in symbol table
 						symbol_table.put(key,value);
 					}
@@ -54,17 +54,17 @@ public class IntermediateCodeGenerator {
 							String key = var;
 							String value[] = new String[3];
 							value[0] = "number";
-							
+
 							//add to symbol table
 							symbol_table.put(key,value);
-							
+
 							// add intermediate code entry
 							intermediate_code.add("declare" + " " + var);
 							i++;
 							String rs = "";
 							while (instructions[i].length()> 1 && instructions[i].split(" ")[0].equals("expression")){
 								// evaluate the expressions and write it to rs
-								
+
 								rs += instructions[i].split(" ")[1]+" "+instructions[i].split(" ")[2]+" ";
 								i++;
 							}
@@ -74,11 +74,36 @@ public class IntermediateCodeGenerator {
 						}
 					}
 				}
-				
+				else if (instructions[i].trim().equals("print_statement")){
+					//Ignore empty cells
+					while (instructions[i] == " ")i++;
+					// goto next valid statement and check if it is declare or declare and assign
+					i++;
+					// print statement will be of 3 types, variable, value or string
+					// split this string and check type of print statement
+					String []print = instructions[i].split(" ");
+
+					// print variable
+					if (print[0].trim().equals("print_var")){					
+						intermediate_code.add(print[1] + "_var " + print[2]);
+					}
+					// print a statement
+					else if (print[0].trim().equals("print_num")){
+						intermediate_code.add(print[1] + "_num " + print[2]);
+					}
+					else if (print[0].trim().equals("print_line")){
+						String []line = instructions[i].split("\"");
+						String quote_to_print = "\"" + line[1]+ "\"";
+
+						intermediate_code.add(print[1] + "_line " +  quote_to_print);
+					}
+
+				}
+
 			}
 		}
 	}
 }
-					
-				
-				
+
+
+
