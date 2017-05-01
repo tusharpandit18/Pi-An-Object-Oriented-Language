38368f16170490870e0dbf4fd120c9ee322cb2e1
Tushar Pandit <tushar.pandit18@gmail.com>
2017-05-01 01:06:48 -0700

    Changed print statement, if_else block
    Changed print statement, if_else block

--- SER502_14/src/IntermediateCodeGenerator.java
+++ SER502_14/src/IntermediateCodeGenerator.java
@@ -6,48 +6,48 @@ import java.util.Stack;
 
 
 public class IntermediateCodeGenerator {
-	
+
 	//static HashMap<String,String[]> symbol_table = new HashMap<String,String[]>();
 	public static void main(String[] args) {
-	
+
 	}
 	public ArrayList<String> generate(String parseTree, String classname){
-		
+
 	HashMap<String,String> symbols = new HashMap<String,String>();
 	symbols.put("+","ADDX");
 	symbols.put("-","SUBX");
 	symbols.put("/","DIVX");
 	symbols.put("*","MULX");
 	// Writer object to generate intermediate code
-	
-		
+
+
 	//String parseTree = "(program (statement (declaration (declare number vara .)) (declaration (declare_and_assign (number_declare_assign number (number_assign varb = (expression 4 + (expression 5 + (expression a .))))))) (declaration (declare boolean c .)) (declaration (declare_and_assign (boolean_declare_assign boolean (bool_assign d = true .)))) (declaration (declare string e .)) (declaration (declare_and_assign (string_declare_assign string (string_assign srr = \"Hello\" .)))) (print_statement (print_var print a .)) (print_statement (print_var print b .)) (print_statement (print_var print c .))))";
 	//String parseTree = Runner.parse_tree;
 	// Split the tree and store in an array
-	String instructions[] = parseTree.split("[()]+"); 
+	String instructions[] = parseTree.split("[()]+");
 	ArrayList<String> intermediate_code = new ArrayList<String>();
-	
+
 	// Key - variable name
 	// array : index-0 - type, index-1 - value, index -3 attribute
 	// value will be assigned in runtime
-	
-	
+
+
 /*	for (String i:instructions){
 		System.out.println(i);
 	}*/
-	
+
 	for (int  i = 0; i< instructions.length ; i++){
-				
+
 		// Ignore empty rows
 		if (! instructions[i].equals("")){
 			// Write functions for each operation
-			
+
 			if (instructions[i].trim().equals("declaration")){
 				//Ignore empty cells if any
 				while (instructions[i] == "")i++;
 				// goto next valid statement and check if it is declare or declare and assign
 				i++;
-								
+
 				if (instructions[i].split(" ")[0].equals("declare")){
 					String []row = instructions[i].split(" ");
 //					String key = row[2];
@@ -56,7 +56,7 @@ public class IntermediateCodeGenerator {
 					//value[1] = "";
 					// enter in intermediate code
 					intermediate_code.add(row[0]+ " " + row[1] +" " + row[2]);
-					
+
 					// enter in symbol table
 //					if (symbol_table.containsKey(key)){
 //						System.out.println("Exception - duplicate variable declaration");
@@ -78,7 +78,7 @@ public class IntermediateCodeGenerator {
 //						String key = var;
 //						String value[] = new String[3];
 //						value[0] = "number";
-						
+
 						//add to symbol table
 //						if (symbol_table.containsKey(key)){
 //							System.out.println("Exception - duplicate variable declaration");
@@ -86,15 +86,15 @@ public class IntermediateCodeGenerator {
 //						else {
 //							symbol_table.put(key,value);
 //						}
-						
+
 						// add intermediate code entry
 						intermediate_code.add("DECL NMBR" + " " + var);
 						i++;
 						String rs = "";
-						
+
 						Stack<String> operatorSt = new Stack<String>();
 						Stack<String> operandSt = new Stack<String>();
-						
+
 						while (instructions[i].length()> 1 && instructions[i].split(" ")[0].equals("expression")){
 							// evaluate the expressions and write it to rs
 							operatorSt.push(instructions[i].split(" ")[2]);
@@ -126,11 +126,11 @@ public class IntermediateCodeGenerator {
 								}
 								intermediate_code.add(symbols.get(operator) + " " + op1 + " " + op2);
 							}
-						
+
 						intermediate_code.add("LOAD "+ var + " ACCM");
 						}
 						// read stack
-						
+
 						// to remove last .
 						//rs = rs.substring(0, rs.length()-2);
 						//intermediate_code.add(var + " = " + rs);
@@ -138,15 +138,15 @@ public class IntermediateCodeGenerator {
 					}
 					else if (instructions[i].split(" ")[0].equals("boolean_declare_assign")){
 						i++;
-						
+
 						String[] row = instructions[i].split(" ");
-						
+
 						//Add declaration
 						intermediate_code.add("DECL BOOL" + " " + row[1]);
 //						String key = row[1];
 //						String value[] = new String[3];
 //						value[0] = "boolean";
-						
+
 						//add to symbol table
 //						if (symbol_table.containsKey(key)){
 //							System.out.println("Exception - duplicate variable declaration");
@@ -162,7 +162,7 @@ public class IntermediateCodeGenerator {
 						i++;
 						String[] row = instructions[i].split(" ");
 						String line_input = instructions[i].split("\"")[1];
-						
+
 //						String key = row[1];
 //						String value[] = new String[3];
 //						value[0] = "string";
@@ -179,8 +179,63 @@ public class IntermediateCodeGenerator {
 						intermediate_code.add("LOAD "+ row[1]+" "+ " \""+ line_input+"\"");
 					}
 				}
-				
+
 			}
+			else if (instructions[i].trim().equals("print_statement")){
+				//Ignore empty cells
+				while (instructions[i] == " ")i++;
+				// goto next valid statement and check if it is declare or declare and assign
+				i++;
+				// print statement will be of 3 types, variable, value or string
+				// split this string and check type of print statement
+				String []print = instructions[i].split(" ");
+
+				// print variable
+				if (print[0].trim().equals("print_var")){
+					intermediate_code.add("PRNT VARX " + print[2]);
+				}
+				// print a statement
+				else if (print[0].trim().equals("print_num")){
+					intermediate_code.add("PRNT NMBR " + print[2]);
+				}
+				else if (print[0].trim().equals("print_line")){
+					String []line = instructions[i].split("\"");
+					String quote_to_print = "\"" + line[1]+ "\"";
+
+					intermediate_code.add("PRNT LINE " +  quote_to_print);
+				}
+
+			}
+			else if (instructions[i].trim().split(" ")[0].equals("if_statement")){
+
+				String keyword_if = "IFST";
+				i++;
+				String []condition = instructions[i].split(" ");
+
+				intermediate_code.add(keyword_if + " " + condition[2]+ " " + condition[1] + " " + condition[3]);
+			}
+			else if (instructions[i].trim().split(" ")[0].equals("else")){
+				intermediate_code.add("ELSE");
+			}
+			else if (instructions[i].trim().split(" ").length >  1 &&
+					(instructions[i].trim().split(" ")[1].equals("endif")||
+					 instructions[i].trim().split(" ")[0].equals("endif"))){
+				intermediate_code.add("EDIF");
+			}
+			else if (instructions[i].trim().split(" ")[0].equals("loop_statement")){
+
+				String keyword_loop = "LOOP";
+				i++;
+				String []condition = instructions[i].split(" ");
+				intermediate_code.add(keyword_loop + " " + condition[2]+ " " + condition[1] + " " + condition[3]);
+			}
+			else if (instructions[i].trim().split(" ").length >  1 &&
+					(instructions[i].trim().split(" ")[1].equals("endwhile")||
+					 instructions[i].trim().split(" ")[0].equals("endwhile"))){
+
+				intermediate_code.add("EDLP");
+			}
+
 }
 	}
 	//System.out.println();
@@ -188,18 +243,18 @@ public class IntermediateCodeGenerator {
 	try{
 		String filename = classname + ".txt";
 		PrintWriter writer = new PrintWriter(filename, "UTF-8");
-		 
+
 		for (String i:intermediate_code){
 			//	System.out.println(i);
 			writer.println(i);
 		}
 		writer.close();
-		 
+
 	} catch (IOException e) {
 		   // do something
 		System.out.println("IOException occurred while writing the file");
 	}
 
 	return intermediate_code;
-  }	
+  }
 }
